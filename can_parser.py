b5f78a88f8836c8c42d399c4911fd28214e16de4
SeungYeob <julio981007@kookmin.ac.kr>
2023-11-07 11:51:45 +0900

    update

--- can_parser.py
+++ can_parser.py
@@ -12,27 +12,27 @@ class Can_parser:
         self.can_data_name = can_data_name
         self.C_db = cantools.database.load_file(can_db_path)
         
-    def get_can_data(self):
-        db_msg=[]
+        self.db_msg=[]
         for msg in self.C_db.messages:
             if msg.name in self.can_msg_list[self.vehicle]:
-                db_msg.append(msg)
+                self.db_msg.append(msg)
         
-        can_data_dict={self.can_data_name[i] : None for i in range(len(self.can_data_name))}
+        self.can_data_dict={self.can_data_name[i] : None for i in range(len(self.can_data_name))}
         
-        filters = [{"can_id" : db_msg[i].frame_id, "can_mask": 0xFFFFFFF} for i in range(len(db_msg))]
+        self.filters = [{"can_id" : self.db_msg[i].frame_id, "can_mask": 0xFFFFFFF} for i in range(len(self.db_msg))]
         
         if vehicle == 'NE':
-            can_bus = can.interface.Bus(channel = 'can0', bustype = 'socketcan',bitrate=500000, data_bitrate=2000000, fd=True, can_filters=filters) # HMC NE
+            self.can_bus = can.interface.Bus(channel = 'can0', bustype = 'socketcan',bitrate=500000, data_bitrate=2000000, fd=True, can_filters=self.filters) # HMC NE
         elif vehicle == 'IONIQ19':
-            can_bus = can.interface.Bus(channel = 'can0', bustype = 'socketcan', can_filters=filters) # KMU
+            self.can_bus = can.interface.Bus(channel = 'can0', bustype = 'socketcan', can_filters=self.filters) # KMU
         else:
             raise NameError("Vehicle name is not correct.")
         
+    def get_can_data(self):
         while True:
-            can_msg = can_bus.recv()
+            can_msg = self.can_bus.recv()
             
-            for msg in db_msg:
+            for msg in self.db_msg:
                 if can_msg.arbitration_id == msg.frame_id:
                     can_dict = self.C_db.decode_message(can_msg.arbitration_id, can_msg.data)
                     can_dict = {k: v for k, v in can_dict.items() if k in self.can_data_name[vehicle]}
@@ -55,4 +55,4 @@ if __name__ == '__main__':
     vehicle = 'NE'
     can_db_path=os.path.join(os.path.dirname(__file__), 'dbc', '230518_NE1_2021_FD_C_v2.dbc')
     can_msg = ['VCU_AccPedDepVal']
-    Can_parser(vehicle=vehicle, can_db_path=can_db_path, can_data_name=can_msg).get_can_data()
+    print(Can_parser(vehicle=vehicle, can_db_path=can_db_path, can_data_name=can_msg).get_can_data())
