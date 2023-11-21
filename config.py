815691e7f870a05a0a991e4b9c249e43b5222430
Geesung Oh <gsethan17@gmail.com>
2023-11-21 12:51:24 +0900

    initial commit for Discriminator Corner Duration Time

---
+++ config.py
@@ -0,0 +1,23 @@
+
+configParam = {
+    # discriminator corner
+    'StrAngOn':10.,     # default 15
+    'StrAngOff':3.,     # default 5
+    'StrAngSpdOn':30.,  # default 50
+    'StrAngSpdOff':15., # default 20
+    'StrPatience':0.5,  # sec
+    
+    # # input range
+    'StrMin':-420.,  # deg
+    'StrMax':+420.,  # deg
+    
+    'VxMin':+10.,  # kph
+    'VxMax':+165.,  # kph
+    
+    'TimeMin':0., # sec
+    'TimeMax':+5., # sec
+    'TimeStep':1e-2,    # 0.01 sec
+    
+    'AyMin':-1.0,   #g
+    'AyMax':+1.0,   #g
+}
