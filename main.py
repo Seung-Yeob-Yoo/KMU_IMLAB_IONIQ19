8cbdfa791ca6c6a9655da762a722909c2f0774b4
imlab <julio981007@kookmin.ac.kr>
2023-11-24 11:08:58 +0900

    code cleaning and add code for shared memory warning at terminate thread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import multiprocessing
-import multiprocessing.shared_memory
+from multiprocessing import shared_memory
 import os
 import argparse
 import numpy as np
@@ -23,37 +23,44 @@ parser.add_argument('--isSave', type=str2bool, default=False,
 args = parser.parse_args()
 '''
 
-def run_discriminator(shared_flag, shared_memory_name, lock, stop_event):
+def run_discriminator(shared_flag, shared_memory_name, stop_event):
+    c = shared_memory.SharedMemory(name=shared_memory_name)
+    shared_flag = np.ndarray((1,), dtype=np.bool_, buffer=c.buf)
+
     discriminator = DiscriminatorCorner()
+    
     for corner_flag in discriminator.run():
         if stop_event.is_set():
+            c.close()
             break
         shared_flag[0] = corner_flag
-        print(f"{corner_flag}       ", end='\r')
     
-def generate_input(shared_flag, shared_memory_name, lock, stop_event):
-    existing_shm = multiprocessing.shared_memory.SharedMemory(name=shared_memory_name)
+def generate_input(shared_flag, shared_memory_name, stop_event):
+    c = shared_memory.SharedMemory(name=shared_memory_name)
+    shared_flag = np.ndarray((1,), dtype=np.bool_, buffer=c.buf)
+
     while True:
         if stop_event.is_set():
+            c.close()
             break
-        existing_shm = np.ndarray((1,), dtype=np.bool_, buffer=existing_shm.buf)
-        print(existing_shm)
+        print(shared_flag, end='\r')
 
 def main():
     procs = []
     stop_event = multiprocessing.Event()
-    # shared_flag = multiprocessing.Value('i', True)
+
     shared_flag_init = np.array([False])
-    shared_flag_arr = multiprocessing.shared_memory.SharedMemory(create=True, size=shared_flag_init.nbytes)
-    shared_memory_name = shared_flag_arr.name
-    shared_flag = np.ndarray(shared_flag_init.shape, dtype=shared_flag_init.dtype, buffer=shared_flag_arr.buf)
-    lock = multiprocessing.Lock()
+    shared_mem = multiprocessing.shared_memory.SharedMemory(create=True, size=shared_flag_init.nbytes)
+    shared_memory_name = shared_mem.name
+    shared_flag = np.ndarray(shared_flag_init.shape, dtype=shared_flag_init.dtype, buffer=shared_mem.buf)
+    shared_flag = shared_flag_init
+    del shared_flag_init
     
     print("[INFO] Main thread started.")
     multiproc_settings = {'DiscriminatorCorner': {'target': run_discriminator,
-                                        'args': (shared_flag, shared_memory_name, lock, stop_event)},
+                                        'args': (shared_flag, shared_memory_name, stop_event)},
                           'GeneratorInput': {'target': generate_input,
-                                        'args': (shared_flag_arr, shared_memory_name, lock, stop_event)},
+                                        'args': (shared_flag, shared_memory_name, stop_event)},
                         }
     
     for key, value in multiproc_settings.items():
@@ -72,8 +79,9 @@ def main():
 
     for proc in procs:
         proc.join()
+    
+    shared_mem.close()
+    shared_mem.unlink()
 
 if __name__ == '__main__':
-    # print(bool(multiprocessing.Value('i', True).value))
-    os.system("sudo ip link set can0 up type can bitrate 500000")
     main()
